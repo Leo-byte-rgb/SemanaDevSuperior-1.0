9fe0c99c91ec5d96cd5a0fda35a0029d00f3a3d9
leo-byte-rgb <leonaradocosta@Gmail.com>
2020-09-14 11:02:01 -0300

    DTO, Service

---
+++ backend/src/main/java/com/devsuperior/dspesquisa/dto/GameDTO.java
@@ -0,0 +1,60 @@
+package com.devsuperior.dspesquisa.dto;
+
+import java.io.Serializable;
+
+import com.devsuperior.dspesquisa.entities.Game;
+import com.devsuperior.dspesquisa.entities.enums.Platform;
+
+public class GameDTO implements Serializable {
+
+	
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String title;
+	private Platform platform;
+	
+	GameDTO(){
+		
+	}
+
+	public GameDTO(Long id, String title, Platform platform) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.platform = platform;
+	}
+	
+	public GameDTO(Game entity)
+	{
+		id = entity.getId();
+		title= entity.getTitle();
+		platform= entity.getPlatform();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTile() {
+		return title;
+	}
+
+	public void setTile(String tile) {
+		this.title = tile;
+	}
+
+	public Platform getPlatform() {
+		return platform;
+	}
+
+	public void setPlatform(Platform platform) {
+		this.platform = platform;
+	}
+	
+}
