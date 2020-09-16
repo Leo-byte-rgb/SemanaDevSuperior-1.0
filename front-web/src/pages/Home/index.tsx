60d3fe689cbe2e10152207c604057cf01dbc587a
leo-byte-rgb <leonaradocosta@Gmail.com>
2020-09-16 15:35:26 -0300

    criando navageção com rotas

--- front-web/src/pages/Home/index.tsx
+++ front-web/src/pages/Home/index.tsx
@@ -2,20 +2,21 @@ import React from 'react';
 import './styles.css';
 import {ReactComponent as SetaIcone} from '../../assets/Seta.svg';
 import {ReactComponent as GamerImage} from '../../assets/gamer.svg';
-
+import { Link } from 'react-router-dom'
 const Home = () => (
 
     <div className="home-container">
         <div className="home-text">
             <h1 className="home-text-title">Quais jogos a galera gosta mais?</h1>
             <h3 className="home-text-subtitle"> Clique no botão abaixo e saiba quais são os jogos que os gamers estão escolhendo!</h3>
-        
+            <Link to="/Records">
             <div className="home-actions">
                 <button className="home-btn">QUERO SABER QUAIS SÃO</button>
                 <div className="home-btn-icon">
                     <SetaIcone/>
                 </div>
             </div>
+            </Link>
             </div>
             <GamerImage className="home-image"/>
     </div>
